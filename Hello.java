528b250b524e155b3ddf43a4c8ca54e5a2df712b
wuesthouse <tim99416@gmail.com>
2022-03-25 14:25:59 +0900

    added what is git?

--- Hello.java
+++ Hello.java
@@ -2,5 +2,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System,out,println("login");
+		System.out.println("logout");
+		System.out.println("Register");
+		System.out.println("What is Git");
 	}
 }
